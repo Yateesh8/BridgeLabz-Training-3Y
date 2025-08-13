a4d8290383e7780624f030482db89ef0a750fe22
Yateesh Gangwar <yateeshgangwar@gmail.com>
2025-08-13 23:24:03 +0530

    adding files to assignment folder 2

---
+++ assignment-Level-2/basicCalculator.java
@@ -0,0 +1,21 @@
+package assignmentLevel2;
+
+import java.util.Scanner;
+
+public class basicCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double number1 = sc.nextDouble();
+        System.out.println("Enter second number: ");
+        double number2 = sc.nextDouble();
+
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
+
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+                + number1 + " and " + number2 + " is " + addition + ", " + subtraction + ", " + multiplication + ", and " + division);
+    }
+}
---
+++ assignment-Level-2/sideOfSquare.java
@@ -0,0 +1,15 @@
+package assignmentLevel2;
+
+import java.util.Scanner;
+
+public class sideOfSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the perimeter of the square: ");
+        double perimeter = sc.nextDouble();
+
+        double side = perimeter / 4;
+
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+    }
+}
